ae951ae6baa2aae3c6e186ea5234c25c880dd0da
Petar Radosevic <petar@wunki.org>
2011-08-10 12:21:40 +0200

    Added language information

--- javascripts/wunki.ui.js
+++ javascripts/wunki.ui.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($) {
-    $('pre').each(function(){
+    $('code').each(function(){
        var title = $(this).attr("class");
        if(title != ''){
            if(title.match('sourceCode')){
                var title = title.split(' ')[1];
            };
-           $(this).prepend('<span class="language">'+title+'</span>');
+           $(this).before('<span class="language">'+title+'</span>');
        };
     });
 
