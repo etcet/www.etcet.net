dc99ec379b81acbde95dc1c4e54ac79cf4b3536e
Petar Radosevic <petar@wunki.org>
2011-09-15 23:05:42 +0200

    Added easing type

--- javascripts/wunki.ui.js
+++ javascripts/wunki.ui.js
@@ -11,5 +11,5 @@ jQuery(document).ready(function($) {
 
     // Timeago
     $("time.timeago").timeago();
-		$().UItoTop({ easingType: 'linear' });
+		$().UItoTop({ easingType: 'easeOutQuart' });
 });
